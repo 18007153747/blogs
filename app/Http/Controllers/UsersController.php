3651b9405a8d49a9285a5dd19b668b6af052dab0
shenhengxin <shenhengxin@qq.com>
2018-04-26 20:16:29 +0800

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
     //页面展示
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    //编辑页面
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+    //更新个人资料操作
+    public function update(UserRequest $request,User $user){
+
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','更新成功');
     }
 }
