5bd39a54900da0e1ada8c15894cf331545d48127
shenhengxin <shenhengxin@qq.com>
2018-04-27 19:36:35 +0800

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 
@@ -18,9 +19,16 @@ class UsersController extends Controller
     }
 
     //更新个人资料操作
-    public function update(UserRequest $request,User $user){
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+        $data = $request->all();
+        if ($request->avatar){
+            $result = $uploader->save($request->avatar,'avatars',$user->id);
+            if ($result){
+                $data['avatar'] = $result['path'];
 
-        $user->update($request->all());
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','更新成功');
     }
 }
