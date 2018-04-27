6c89ac3ab879f344bb4a50a65d9acdec48fd6baf
shenhengxin <shenhengxin@qq.com>
2018-04-27 20:20:40 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,12 @@ use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    //加载中间件过滤
+    public function __construct()
+    {
+        $this->middleware('auth',['except' => ['show']]);
+    }
+
     //页面展示
     public function show(User $user){
         return view('users.show',compact('user'));
@@ -15,11 +21,13 @@ class UsersController extends Controller
 
     //编辑页面
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     //更新个人资料操作
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+        $this->authorize('update',$user);
         $data = $request->all();
         if ($request->avatar){
             $result = $uploader->save($request->avatar,'avatars',$user->id);
